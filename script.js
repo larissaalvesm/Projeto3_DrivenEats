4e0469a5ffba83311b0ffd497033583ee03641aa
larissa <larissaalves1102@gmail.com>
2023-01-05 15:01:35 -0300

    Seleção de itens implementada com o icon de check

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function selecionarPrato(cardClicado) {
     console.log(cardSelecionadoAnteriormente);
 
     if (cardSelecionadoAnteriormente !== null) {
+        document.querySelector('.cards-prato .selecionado ion-icon').classList.add('escondido');
         cardSelecionadoAnteriormente.classList.remove('selecionado');
     }
 
     const card = document.querySelector(cardClicado);
     console.log(cardClicado);
     card.classList.add('selecionado');
+
+    const check = document.querySelector('.cards-prato .selecionado ion-icon');
+    console.log(check);
+    check.classList.remove('escondido');
 }
 
 function selecionarBebida(cardClicado) {
@@ -18,12 +23,17 @@ function selecionarBebida(cardClicado) {
     console.log(cardSelecionadoAnteriormente);
 
     if (cardSelecionadoAnteriormente !== null) {
+        document.querySelector('.cards-bebida .selecionado ion-icon').classList.add('escondido');
         cardSelecionadoAnteriormente.classList.remove('selecionado');
     }
 
     const card = document.querySelector(cardClicado);
     console.log(cardClicado);
     card.classList.add('selecionado');
+
+    const check = document.querySelector('.cards-bebida .selecionado ion-icon');
+    console.log(check);
+    check.classList.remove('escondido');
 }
 
 function selecionarSobremesa(cardClicado) {
@@ -32,10 +42,15 @@ function selecionarSobremesa(cardClicado) {
     console.log(cardSelecionadoAnteriormente);
 
     if (cardSelecionadoAnteriormente !== null) {
+        document.querySelector('.cards-sobremesa .selecionado ion-icon').classList.add('escondido');
         cardSelecionadoAnteriormente.classList.remove('selecionado');
     }
 
     const card = document.querySelector(cardClicado);
     console.log(cardClicado);
     card.classList.add('selecionado');
+
+    const check = document.querySelector('.cards-sobremesa .selecionado ion-icon');
+    console.log(check);
+    check.classList.remove('escondido');
 }
